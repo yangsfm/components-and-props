9e379826e9700f497b5ed9bda60eec43ff02c590
Yang Sarmento Ferreira <60950025+yangsfm@users.noreply.github.com>
2024-03-12 09:44:40 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Nome from './components/Nome'
+
+function App(){
+  return(
+  <div>
+    <h1>Componente App</h1>
+    <Nome aluno ="Lucas" idade={30} />
+    <br/>
+    <Nome aluno ="José Silva" idade={25} />
+  </div>
+  );
+}
+
+export default App;  
+
